b152e02243552fc1145fe189ca4281ca19fc4e54
MartijnvdLans <74150653+MartijnvdLans@users.noreply.github.com>
2020-11-25 16:03:42 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,16 @@
 var animationBalk = document.getElementById('animation');
 var heart = document.getElementById('filled');
 var button = document.getElementById('button');
+var menu = document.getElementById('menu');
+var nav = document.getElementById('navigatie');
+var close = document.getElementById('cross');
+var open = false;
+var dicht = true;
 
 function animation() {
     animationBalk.classList.remove('right_two');
     setTimeout(animationTwo, 5000);
-};
+}
 
 function animationTwo() {
     animationBalk.classList.add('right');
@@ -23,7 +28,26 @@ function animationThree() {
 function add(event) {
     event.preventDefault();
     heart.classList.toggle('none');
-}
+};
+
+function menuToggle() {
+    if (open == true) {
+        nav.classList.remove('open');
+        nav.classList.add('close');
+        open = false;
+        dicht = true;
+    }
+    
+    else if (dicht == true) {
+        nav.classList.add('open');
+        nav.classList.remove('close');
+        open = true;
+        dicht = false;
+    }
+};
+
 animation();
 
+close.addEventListener('click', menuToggle);
+menu.addEventListener('click', menuToggle);
 button.addEventListener('click', add);
