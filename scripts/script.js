2bc018be6d1903ac9a3a27b9a3285fc3bba1bcb9
MartijnvdLans <74150653+MartijnvdLans@users.noreply.github.com>
2020-11-25 13:29:58 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,29 @@
 // JavaScript Document
+
+var animationBalk = document.getElementById('animation');
+var heart = document.getElementById('filled');
+var button = document.getElementById('button');
+
+function animation() {
+    animationBalk.classList.remove('right_two');
+    setTimeout(animationTwo, 5000);
+};
+
+function animationTwo() {
+    animationBalk.classList.add('right');
+    setTimeout(animationThree, 5000);
+};
+
+function animationThree() {
+    animationBalk.classList.remove('right');
+    animationBalk.classList.add('right_two');
+    setTimeout(animation, 5000);
+};
+
+function add(event) {
+    event.preventDefault();
+    heart.classList.toggle('none');
+}
+animation();
+
+button.addEventListener('click', add);
